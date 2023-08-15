11acbc04e76b0f0da89eb5aaf5e42b82c902f523
unknown <shishkina.ja@gmail.com>
2023-08-15 13:51:57 +0200

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
 
+    }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max
     }
-
+    
     guess() {
+        this.guessNumber = Math.round((this.max + this.min)/2)
 
+    //предполагать()
+    //этот метод возвращает кандидата решения (вы делаете предположение на основе диапазона и предыдущих предположений)
+        return this.guessNumber
     }
-
     lower() {
-
+      return this.max = this.guessNumber
+        
+        //этот метод вызывается, если предыдущая функция предположения() вернула число, которое больше, чем ответ
     }
-
+    
     greater() {
-
+      return this.min = this.guessNumber
+        //этот метод вызывается, если предыдущая функция предположения() вернула число меньше, чем ответ
     }
 }
+
 
 module.exports = GuessingGame;
